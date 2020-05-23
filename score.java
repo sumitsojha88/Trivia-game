cb53b0f977cba57e261476d1d032f5cd9e34e3dd
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2020-05-24 00:30:09 +0530

    Create score.java

---
+++ score.java
@@ -0,0 +1,17 @@
+package model;
+
+public class score {
+
+    private int score;
+
+    public score() {
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
