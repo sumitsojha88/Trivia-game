d44b84b6ee801fb8e3b082febb72e07c71e08400
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2020-06-12 14:59:01 +0530

    Create multiscreen orderactivity.java

---
+++ multiscreen orderactivity.java
@@ -0,0 +1,27 @@
+package com.codewithharry.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+
+        // Set text inside textview to message
+        TextView textView = findViewById(R.id.orderText);
+        textView.setText(message);
+
+
+
+    }
+}
+
