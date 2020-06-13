589e49cdf7a0159c735fc7c99c99779730a7266a
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2020-06-13 16:21:44 +0530

    Update musicplayer android.java

--- musicplayer android.java
+++ musicplayer android.java
@@ -1,14 +1,21 @@
-package com.codewithharry.musicxdev;
+package com.example.sojhamusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
-    // for playing the music
+    AudioManager audioManager;
+
     public void play(View view){
         player.start();
     }
@@ -27,7 +34,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         player = MediaPlayer.create(this, R.raw.music);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+
+        SeekBar seekVol = findViewById(R.id.seekVol);
+        seekVol.setMax(maxVol);
+        seekVol.setProgress(curVol);
+
+        seekVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar seekProg = findViewById(R.id.seekProg);
+        seekProg.setMax(player.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekProg.setProgress(player.getCurrentPosition());
+            }
+        }, 0 , 900);
+
+        seekProg.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 }
 
